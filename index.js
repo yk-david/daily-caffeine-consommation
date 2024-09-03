e0268c3358be478ee276c4155ffa4523ca0b1a0b
yk-david <81497136+yk-david@users.noreply.github.com>
2024-09-03 14:47:31 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el");
+let count = 0;
+
+let cafAmericano = 150;
+let cafLatte = 75;
+let cafTea = 25;
+
+function incAmericano() {
+    count += cafAmericano;
+    countEl.textContent = count;
+    saveEl.textContent += 'Americano - ';
+}
+
+function incLatte() {
+    count += cafLatte;
+    countEl.textContent = count;
+    saveEl.textContent += 'Cafe Latte - ';
+}
+
+function incTea() {
+    count += cafTea;
+    countEl.textContent = count;
+    saveEl.textContent += 'Black Tea - ';
+}
+
